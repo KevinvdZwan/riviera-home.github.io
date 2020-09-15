5fff9b79ee994236f8d0caa806e55d28eb21d545
KevinvdZwan <302147728@student.rocmondriaan.nl>
2020-09-15 11:05:40 +0200

    Push

--- Rviiera/riviera-home.nl/application/files/cache/js/form.js
+++ Rviiera/riviera-home.nl/application/files/cache/js/form.js
@@ -1,33 +1,36 @@
 console.log('It works');
 $(document).ready(function () {
     $('.submit').click(function (evt) {
-        evt.preventDefault();
         console.log('Clicked button');
+        var salutation = $.('.salutation').val();
         var name = $('.name').val();
         var email = $('.email').val();
-        var subject = $('.subject').val();
         var message = $('.message').val();
         var statusElm = $('.status');
         statusElm.empty();
-        if (name.length > 2) {
-            statusElm.append('<div>Name is valid</div>');
+        if (document.getElementsByName('salutation').checked) {
+            //Radio button is checked
         } else {
-            statusElm.append('<div>Name is not valid</div>');
+            evt.preventDefault();
+            statusElm.append('<div>Kruis een aanhef aan!</div>')
+        }
+        if (name.length > 2) {
+            statusElm.append('<div>Naam staat goed</div>');
+        } else {
+            evt.preventDefault();
+            statusElm.append('<div>Naam staat niet goed</div>');
         }
         if (email.length > 5 && email.includes('@') && email.includes('.')) {
-            statusElm.append('<div>Email is valid</div>');
+            statusElm.append('<div>Email staat goed</div>');
         } else {
-            statusElm.append('<div>Email is not valid</div>');
-        }
-        if (subject.length > 2) {
-            statusElm.append('<div>Subject is valid</div>');
-        } else {
-            statusElm.append('<div>Subject is not valid</div>');
+            evt.preventDefault();
+            statusElm.append('<div>Email staat niet goed</div>');
         }
         if (message.length > 20) {
-            statusElm.append('<div>Message is valid</div>');
+            statusElm.append('<div>Bericht staat goed</div>');
         } else {
-            statusElm.append('<div>Message is not valid</div>');
+            evt.preventDefault();
+            statusElm.append('<div>Bericht staat niet goed</div>');
         }
     });
 });
